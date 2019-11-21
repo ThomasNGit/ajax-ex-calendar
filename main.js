4030499a9830f117033c8638317abfbaa3297b52
ThomasNGit <neronithomas1989@gmail.com>
2019-11-21 11:08:54 +0100

    reformatted the code and added the data attribute to make the holiday red-colored

--- main.js
+++ main.js
@@ -1,52 +1,38 @@
-$( document ).ready(function(){
+// Controllare quanti giorni ha il mese  formando così una lista;
+// Chiedere all’api quali sono le festività per il mese ;
+// Evidenziare le festività nella lista
+$(document).ready(function () {
+
 
     $.ajax({
-
-        url : "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
-
-        method : "GET",
-
-        success : function(data){
-            console.log(data.response);
-            
-            for(j = 0; j < data.response.length; j++)
-
-            var festa = data.response[j]
-
-            giornoDiFesta = festa.date;
-
-            console.log(giornoDiFesta);
-            
-            $(".festa").append(giornoDiFesta);
-
-                // var data = moment("2018-01").day("1").month("0").year("2018").format("dddd DD/MMMM/YYYY");
-            var daysNumber = moment("01/01/2018", "DD/MM/YYYY").daysInMonth();
-
-            console.log(daysNumber);
-
-            var monthContainer = $(".mese");
-
-            for(var i = 1; i <= daysNumber; i++){
-
-                var giorno = moment("2018-01-" + i).format("YYYY-MM-DD")
-
-                console.log(giorno);
-                
-                monthContainer.append("<li>" + giorno + "</li>")
-
-                if(giornoDiFesta === giorno){
-
-                    giorno.addClass("holiday")
-                }
-                    
-
-            }
-
-
-        }
-
-    })
-
-
-
-})
+       url: "https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0",
+       method: "GET",
+       success: function (data) {
+          console.log(data.response)
+ 
+          var daysNumber = moment("2018-01-01", "YYYY-MM-DD").daysInMonth();
+          
+          for (i = 1; i <= daysNumber; i++) {
+             var dayToday = "2018-01-" + i;
+             var dateFormat = moment(dayToday).format("YYYY-MM-DD")
+             var currentDay = moment(dayToday).format("dddd D MMMM");
+             $(".mese").append('<li data-date="' + dateFormat +'">'+ currentDay +'</li>');
+          }
+          for (var i = 0; i < data.response.length; i++) {
+             console.log(data.response[i].date)
+ 
+             var feste = data.response[i].date  
+             $(".mese [data-date='" + feste + "']").css("color","red").append(" "+data.response[i].name)
+                        
+          }
+ 
+          
+ 
+ 
+       },
+       error: function () {
+          alert("Error");
+       }
+    });
+ 
+ })
